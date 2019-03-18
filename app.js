cb1501d94df5cfb07b49a72d86a890e4315fdcb4
Andres F Muñoz Echeverria <andresmecheverria@gmail.com>
2019-03-17 19:57:11 -0500

    wedding barbie charlie

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var compression = require('compression');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false 
+}));
+app.use(cookieParser());
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'app/images', 'ico_babi_charlie.ico')));
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.use('/', index);
+
+//Headers
+app.use(function (req, res, next) {
+
+    var origin = req.headers.origin;
+
+    if (origin) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+
+
+module.exports = app;
