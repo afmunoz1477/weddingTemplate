ed7d4fd640b6879b60b5f7095e252d03931979f8
Andres F Muñoz Echeverria <andresmecheverria@gmail.com>
2019-03-20 08:22:59 -0500

    things to do.... pending RSVP

--- app/scripts/controller/homeCtrl.js
+++ app/scripts/controller/homeCtrl.js
@@ -3,7 +3,6 @@
 
 var app = angular.module('Babie&Charlie');
 app.controller('homeCtrl', function ($scope, $state, $location, $http) {
-
   $scope.w3_open = function w3_open() {
     var x = document.getElementById("mySidenav");
     var y = document.getElementById("completePage");
@@ -145,6 +144,36 @@ app.controller('homeCtrl', function ($scope, $state, $location, $http) {
 
     // Add smooth scrolling on all links inside the navbar
     $("#contactanos a").on('click', function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        console.log("header")
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 500, function () {
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+  
+  $(document).ready(function () {
+    // Add scrollspy to <body>
+    $('body').scrollspy({
+      target: ".navbar",
+      offset: 50
+    });
+
+    // Add smooth scrolling on all links inside the navbar
+    $("#buttonup a").on('click', function (event) {
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
         // Prevent default anchor click behavior
